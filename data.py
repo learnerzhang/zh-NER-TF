ce822a529f3e00a2d5c5ed6648c56f15cbb8a085
zhangzhen <zhangzhen@mininglamp.com>
2019-03-05 18:08:40 +0800

    lic2019

--- data.py
+++ data.py
@@ -2,11 +2,19 @@ import sys, pickle, os, random
 import numpy as np
 
 ## tags, BIO
-tag2label = {"O": 0,
-             "B-PER": 1, "I-PER": 2,
-             "B-LOC": 3, "I-LOC": 4,
-             "B-ORG": 5, "I-ORG": 6
-             }
+# tag2label = {"O": 0,
+#              "B-PER": 1, "I-PER": 2,
+#              "B-LOC": 3, "I-LOC": 4,
+#              "B-ORG": 5, "I-ORG": 6
+#              }
+
+tag2label = {'O': 0, 'B-WLXS': 1, 'I-WLXS': 2, 'B-QY': 3, 'I-QY': 4, 'B-NUM': 5, 'I-NUM': 6, 'B-DSZY': 7, 'I-DSZY': 8,
+             'B-DD': 9, 'I-DD': 10, 'B-JG': 11, 'I-JG': 12, 'B-YY': 13, 'I-YY': 14, 'B-GQ': 15, 'I-GQ': 16, 'B-CS': 17,
+             'I-CS': 18, 'B-RW': 19, 'I-RW': 20, 'B-SJ': 21, 'I-SJ': 22, 'B-CBS': 23, 'I-CBS': 24, 'B-XZQ': 25,
+             'I-XZQ': 26, 'B-TEXT': 27, 'I-TEXT': 28, 'B-YSZP': 29, 'I-YSZP': 30, 'B-YYZJ': 31, 'I-YYZJ': 32,
+             'B-XX': 33, 'I-XX': 34, 'B-DATE': 35, 'I-DATE': 36, 'B-TSZP': 37, 'I-TSZP': 38, 'B-SW': 39, 'I-SW': 40,
+             'B-XKZY': 41, 'I-XKZY': 42, 'B-JD': 43, 'I-JD': 44, 'B-WZ': 45, 'I-WZ': 46, 'B-M': 47, 'I-M': 48,
+             'B-QH': 49, 'I-QH': 50, 'B-ZP': 51, 'I-ZP': 52, 'B-LSRW': 53, 'I-LSRW': 54, 'B-GJ': 55, 'I-GJ': 56}
 
 
 def read_corpus(corpus_path):
@@ -21,7 +29,7 @@ def read_corpus(corpus_path):
     sent_, tag_ = [], []
     for line in lines:
         if line != '\n':
-            [char, label] = line.strip().split()
+            [char, label] = line.strip().split("\t")
             sent_.append(char)
             tag_.append(label)
         else:
@@ -45,10 +53,10 @@ def vocab_build(vocab_path, corpus_path, min_count):
         for word in sent_:
             if word.isdigit():
                 word = '<NUM>'
-            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
+            elif ('\u0041' <= word <= '\u005a') or ('\u0061' <= word <= '\u007a'):
                 word = '<ENG>'
             if word not in word2id:
-                word2id[word] = [len(word2id)+1, 1]
+                word2id[word] = [len(word2id) + 1, 1]
             else:
                 word2id[word][1] += 1
     low_freq_words = []
@@ -121,7 +129,7 @@ def pad_sequences(sequences, pad_mark=0):
     :param pad_mark:
     :return:
     """
-    max_len = max(map(lambda x : len(x), sequences))
+    max_len = max(map(lambda x: len(x), sequences))
     seq_list, seq_len_list = [], []
     for seq in sequences:
         seq = list(seq)
@@ -159,3 +167,6 @@ def batch_yield(data, batch_size, vocab, tag2label, shuffle=False):
     if len(seqs) != 0:
         yield seqs, labels
 
+
+if __name__ == '__main__':
+    vocab_build(vocab_path="./lic2019/word2id.pkl", corpus_path="./lic2019/train_data", min_count=3)
