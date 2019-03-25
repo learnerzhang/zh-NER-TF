fd850e2f67b910448e9c8f16eff127e6527902c2
zhangzhen <zhangzhen@mininglamp.com>
2019-03-25 17:57:19 +0800

    opt

--- data.py
+++ data.py
@@ -8,13 +8,8 @@ import numpy as np
 #              "B-ORG": 5, "I-ORG": 6
 #              }
 
-tag2label = {'O': 0, 'B-WLXS': 1, 'I-WLXS': 2, 'B-QY': 3, 'I-QY': 4, 'B-NUM': 5, 'I-NUM': 6, 'B-DSZY': 7, 'I-DSZY': 8,
-             'B-DD': 9, 'I-DD': 10, 'B-JG': 11, 'I-JG': 12, 'B-YY': 13, 'I-YY': 14, 'B-GQ': 15, 'I-GQ': 16, 'B-CS': 17,
-             'I-CS': 18, 'B-RW': 19, 'I-RW': 20, 'B-SJ': 21, 'I-SJ': 22, 'B-CBS': 23, 'I-CBS': 24, 'B-XZQ': 25,
-             'I-XZQ': 26, 'B-TEXT': 27, 'I-TEXT': 28, 'B-YSZP': 29, 'I-YSZP': 30, 'B-YYZJ': 31, 'I-YYZJ': 32,
-             'B-XX': 33, 'I-XX': 34, 'B-DATE': 35, 'I-DATE': 36, 'B-TSZP': 37, 'I-TSZP': 38, 'B-SW': 39, 'I-SW': 40,
-             'B-XKZY': 41, 'I-XKZY': 42, 'B-JD': 43, 'I-JD': 44, 'B-WZ': 45, 'I-WZ': 46, 'B-M': 47, 'I-M': 48,
-             'B-QH': 49, 'I-QH': 50, 'B-ZP': 51, 'I-ZP': 52, 'B-LSRW': 53, 'I-LSRW': 54, 'B-GJ': 55, 'I-GJ': 56}
+tag2label = {'O': 0, 'B-NUM': 1, 'I-NUM': 2, 'B-OTH': 3, 'I-OTH': 4, 'B-PER': 5, 'I-PER': 6, 'B-TXT': 7, 'I-TXT': 8,
+             'B-LOC': 9, 'I-LOC': 10, 'B-PRO': 11, 'I-PRO': 12, 'B-DAT': 13, 'I-DAT': 14, 'B-ORG': 15, 'I-ORG': 16}
 
 
 def read_corpus(corpus_path):
